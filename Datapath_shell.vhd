6ffedd85b382b06789103243df2d2ba9b04659cf
aaronstolze <C16Aaron.Stolze@usafa.edu>
2014-04-14 07:51:51 -0600

    Datapath

--- Datapath_shell.vhd
+++ Datapath_shell.vhd
@@ -55,25 +55,15 @@ architecture Datapath of Datapath is
 
 	-- Copy the declaration for your ALU here
 
+component ALU
+    PORT(
+         OpSel : IN  std_logic_vector(2 downto 0);
+         Data : IN  std_logic_vector(3 downto 0);
+         Accumulator : IN  std_logic_vector(3 downto 0);
+         Result : OUT  std_logic_vector(3 downto 0)
+        );
+    end component ;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-	
 	-- Internal signals for connecting the Datapath registers.  Note the differing length 
 	-- based on content
 	signal  MARHi, MARLo, Accumulator, ALU_Result : std_logic_vector(3 downto 0);
@@ -106,67 +96,69 @@ begin
 	-- asynchronous Reset_L line and clocked data input.  Which control signal also determines
 	-- when data is loaded?  What are the inputs and outputs from the register?
 	
-	process(          )
-  	begin				 
-	  
-
-
-
-
-
-
-  	end process;   
+process(IRLd, Clock, Reset_L)
+		begin				 
+		  if(Reset_L = '0') then
+				IR <= "0000";
+			elsif (Clock'event and Clock='1') then
+				if (IRLd = '1') then
+					IR <= Data;
+				end if;
+		  end if;
+		end process;  
 	  	
 	  	
 	-- Complete the code to implement an Memory Address Register (Hi).  Use a standard register with an 
 	-- asynchronous Reset_L line and clocked data input.  Which control signal also determines
 	-- when data is loaded?	 What are the inputs and outputs from the register?
 
-	process(          )
+	process(Reset_L, Clock, MARHiLd)
   	begin				 
-	  
-
-
-
-
-
-
-  	end process;       
-
+	  if(Reset_L = '0') then
+			MARHi <= "0000";
+		elsif (Clock'event and Clock='1') then
+			if(MARHiLd = '1') then
+				MARHi <= Data;
+			end if;
+	  end if;
+  	end process;   
 	-- Complete the code to implement an Memory Address Register (Lo).  Use a standard register with an 
 	-- asynchronous Reset_L line and clocked data input.  Which control signal also determines
 	-- when data is loaded?	 What are the inputs and outputs from the register?
 	
-	process(          )
+	process(Reset_L, Clock, MARLoLd)
   	begin				 
-	  
-
-
-
-
-
-
-  	end process;   
-	  
+	  if(Reset_L = '0') then
+			MARLo <= "0000";
+	  elsif(Clock'event and Clock='1') then
+			if(MARLoLd = '1') then
+				MARLo <= Data;
+			end if;
+	  end if;
+	end process;  
 	-- Complete the code to implement an Address Selector (multiplexer) which determines between two data sources
 	-- (which two?) based on the AddrSel line. Be careful - the process sensitivity list has 4 signals!
 	
-	process(          )
+process(PC, MARLo, MARHi, AddrSel)
   	begin				 
-	  
+	  if(AddrSel = '1') then
+			Addr(7 downto 4) <= MARHi;
+			Addr(3 downto 0) <= MARLo;
+	  elsif(AddrSel = '0') then
+			Addr <= PC;
+	  end if;
+	end process;   
 
-
-
-
-
-
-  	end process;   
-		
 	
 	  		
 	-- Instantiate and connect the ALU  which was written in a separate file
 	
-
+Inst_ALU: ALU PORT MAP(
+		OpSel => OpSel,
+		Data => Data,
+		Accumulator => Accumulator,
+		Result => ALU_Result
+	);	
 
 
 
@@ -174,25 +166,25 @@ begin
 	-- Complete the code to implement an Accumulator.  Use a standard register with an 
 	-- asynchronous Reset_L line and clocked data input.  Which control signal also determines
 	-- when data is loaded?	   What are the inputs and outputs from the register?
-	process(          )
+process(Reset_L, Clock, AccLd)
   	begin				 
-	  
-
-
-
-
-
-
-  	end process;     
+	  if(Reset_L = '0') then
+			Accumulator <= "0000";
+	  elsif(Clock'event and Clock='1') then
+			if(AccLd = '1') then
+				Accumulator <= ALU_Result;
+			end if;
+	  end if;
+  	end process;      
 	  
 	-- Complete the code to implement a tri-state buffer which places the Accumulator data on the 
 	-- Data Bus when enabled and goes to High Z the rest of the time	
 	-- Note: use "Z" just like a bit.  If you want to set a signal to  High Z, you'd say mySignal <= 'Z';
-	Data <=          when             else         ;
+		Data <= Accumulator when (EnAccBuffer = '1') else ("ZZZZ");
 	  
   	-- Complete the code to implement the Datapath status signals --
-   	AlessZero <=   			--Uses MSB as a sign bit
-  	AeqZero <= 
+   	AlessZero <= Accumulator(3);  --Uses MSB as a sign bit
+  	AeqZero <= '1' when (Accumulator = "0000");
 
 			   
 			   
